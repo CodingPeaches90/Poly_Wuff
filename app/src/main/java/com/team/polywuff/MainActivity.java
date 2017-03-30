ff564872a21e195bca9e1ebe439eb9b774fe7f0e
emmajeng <x15575767@gmail.com>
2017-03-30 11:21:18 +0100

    Removing bottom navigation view

--- app/src/main/java/com/team/polywuff/MainActivity.java
+++ app/src/main/java/com/team/polywuff/MainActivity.java
@@ -1,15 +1,11 @@
 package com.team.polywuff;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -54,8 +50,6 @@ errors
             //startActivity(new Intent(this,FacebookLogin.class));
         }
 */
-
-
     }
 
     // Logout button TEMP!
@@ -72,14 +66,6 @@ errors
         Intent intent = new Intent(this,FacebookLogin.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
-    }
-
-    //this inflates the action bar --> main menu
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
     }
 
 }
