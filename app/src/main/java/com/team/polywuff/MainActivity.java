74f50f10591b6d3f3466f924e0061546d5cffaa1
emmajeng <x15575767@gmail.com>
2017-03-31 19:31:27 +0100

    Added drawer navigation, and linked logout button to getMeOut()

--- app/src/main/java/com/team/polywuff/MainActivity.java
+++ app/src/main/java/com/team/polywuff/MainActivity.java
@@ -2,58 +2,65 @@ package com.team.polywuff;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Firebase variables
     private FirebaseAuth firebaseAuth;
-    private TextView viewEmail;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // our Main Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // we can fetch users details by making a firebase user object
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        /*if(firebaseAuth.getCurrentUser() == null || AccessToken.getCurrentAccessToken() == null){
-            finish();
-            startActivity(new Intent(this,FacebookLogin.class));
-        }
-        // if
-
-        if(AccessToken.getCurrentAccessToken() == null){
-            backToLogin();
-        }*/
-        if (user == null){
+        if (user == null)
+        {
             backToLogin();
         }
 
-        /*viewEmail = (TextView) findViewById(R.id.textView);
-        viewEmail.setText("Welcome " + user.getEmail());
-*/
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-/*
-errors
-        // if token is nothing then go back to login screen!
-        if(AccessToken.getCurrentAccessToken() == null || user == null){
-            backToLogin();
-            //startActivity(new Intent(this,FacebookLogin.class));
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-*/
     }
 
     // Logout button TEMP!
-    public void getMeOut(View v){
+    public void getMeOut(){
         // Facebook already has a logout method
         // which can be called anywhere in our application!
         LoginManager.getInstance().logOut();
@@ -68,4 +75,48 @@ errors
         startActivity(intent);
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the action
+        } else if (id == R.id.nav_profile) {
+            //this is the code for opening activities
+
+            //Intent pIntent = new Intent(this, Profile.class);
+            //startActivity(pIntent);
+
+        } else if (id == R.id.nav_search) {
+
+        } else if (id == R.id.nav_messenger) {
+
+        } else if (id == R.id.nav_map) {
+
+        } else if (id == R.id.nav_pedometer) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_logout) {
+            //brings the user back to the login activity
+            getMeOut();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
